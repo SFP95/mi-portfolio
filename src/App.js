254b9b4d3ae67bd90c7bb130cd0c08b2fa6f9a21
NIa <dam.sonia.fernandez@gmail.com>
2025-05-25 20:07:40 +0200

    Mis cambios locales

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <div className="main-container">
+      <div className="/mi-portfolio">
         <HeroSection />
         <ContainerContent />
       </div>
