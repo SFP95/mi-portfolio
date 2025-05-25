19fa5c93608194d38a6f65ef498126894b7aa7ab
NIa <dam.sonia.fernandez@gmail.com>
2025-05-25 19:56:29 +0200

    upload for deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from './components/HeroSection/HeroSection';
 import ContainerContent from './components/ContainerContext/ContainerContent';
 import './App.css';
 function App() {
   return (
-    <div className="main-container">
-      <HeroSection />
-      <ContainerContent />
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="main-container">
+        <HeroSection />
+        <ContainerContent />
+      </div>
+    </Router>
   );
 }
 
